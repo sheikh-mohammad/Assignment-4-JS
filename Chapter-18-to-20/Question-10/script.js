b6ece106f912ac5ce2deceefbde1e5e254e27c35
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-14 03:23:14 +0500

    Add solution for Question 10: Find smallest number in array

---
+++ Chapter-18-to-20/Question-10/script.js
@@ -0,0 +1,11 @@
+let A = [24, 53, 78, 91, 12];
+let smallestNum = A[0];
+
+for (let i = 0; i < A.length; i++) {
+    if (smallestNum >= A[i]) {
+        smallestNum = A[i];
+    }
+}
+
+document.write(`Array items: ${A} <br/>`);
+document.write(`The smallest number is ${smallestNum}`);
