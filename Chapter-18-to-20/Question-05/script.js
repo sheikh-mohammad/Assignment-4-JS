f2d0719858983af6c8bab195ef94ec74afd8deb3
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-13 08:16:44 +0500

    Add fruit array iteration and index display in Question-05

---
+++ Chapter-18-to-20/Question-05/script.js
@@ -0,0 +1,11 @@
+let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    document.write(`${fruits[i]} <br/>`);
+}
+
+document.write("<br />");
+
+for (let i = 0; i < fruits.length; i++) {
+    document.write(`Element at index ${i} is ${fruits[i]} <br/>`);
+}
